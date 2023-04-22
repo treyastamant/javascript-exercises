5591dface2d5121d44e6710d36d7a99e26bf95ba
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-04-21 21:03:04 -0600

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let rString = "";
+    if (num < 0) {
+        rString = "ERROR";
+    } else {
+        for (let i = 0; i < num; i++) {
+            rString += string;
+        }
+    }
+    return rString;
 };
 
 // Do not edit below this line
