4f286e434443b5541244399269b4b4ea70d50a7e
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-04-21 21:24:20 -0600

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let rString = "";
+    for (let i = string.length; i >= 0; i--) {
+        rString += string.charAt(i);
+    }
+    return rString;
 };
 
 // Do not edit below this line
