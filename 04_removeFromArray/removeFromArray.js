ca474a9d9993925e8ce0adecbe7bfc4f44c1c797
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-04-22 08:30:22 -0600

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (let element of args) {
+        let x = array.indexOf(element);
+        if (x !== -1) array.splice(x, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
