538818fad6156aa15d18c0e8a3f562c235c8e983
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-04-22 15:35:25 -0600

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,21 @@
 const sumAll = function(int1, int2) {
-    let sum = 0;
-    if (int1 < int2) {
-        for (i = int1; i <= int2; i++) {
-        sum += i;
-        }  
-    } else if (int2 < int1) {
-        for (i = int2; i <= int1; i++) {
-            sum += i;
-        }  
-    }
-    return sum;
+	let sum = 0;
+	let ty1 = typeof int1;
+	let ty2 = typeof int2;
+	if ((int1 || int2) < 0) {
+		sum = "ERROR";
+	} else if (ty1 !== 'number' || ty2 !== 'number') {
+		sum = "ERROR";
+	}	else if (int2 < int1) {
+		for (i = int2; i <= int1; i++) {
+			sum += i;
+		}	
+	} else if (int1 < int2) {
+		for (i = int1; i <= int2; i++) {
+			sum += i;
+		}   
+	}
+		return sum;
 };
 
 // Do not edit below this line
