e3140ce1879c8837be862c92740a1a296d3345d4
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-04-22 21:23:59 -0600

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,6 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = (f) => Math.round((f - 32) * (5 / 9) * 10) / 10;
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = (c) => Math.round(((c * 9) / 5 + 32) * 10) / 10;
 
 // Do not edit below this line
 module.exports = {
