5cc9bc153e48de069f6e453963c70a4548c4697b
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-05-11 20:26:27 -0600

    Polish code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,17 +1,10 @@
 const fibonacci = function(num) {
-let i;
 let fib = [0, 1];
-if (num < 0) {
-  return "OOPS";
-} else {
-  for (i = fib.length; i <= num; i++) {
-    fib[i] = fib[i - 2] + fib[i - 1];
-    console.log(fib);
-  }
+if (num < 0) return "OOPS";
+for (let i = fib.length; i <= num; i++) {
+  fib[i] = fib[i - 2] + fib[i - 1];
 }
 return fib[num];
-
-
 };
 
 // Do not edit below this line
