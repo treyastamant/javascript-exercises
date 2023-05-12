cd25801c8204e1c40cb884ec4d372f914ff5b826
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-05-11 22:23:00 -0600

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let currentYear =  new Date().getFullYear();
+  let ages = [];
+  let megaMax;
+  for (let i = 0; i < people.length; i++) {
+    let age = (people[i].yearOfDeath || currentYear) - people[i].yearOfBirth;
+    ages.push(age);
+    const max = Math.max(...ages);
+    if (max === age) megaMax = people[i];
+  }
+  return megaMax;
 };
 
 // Do not edit below this line
