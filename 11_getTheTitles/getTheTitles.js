0477e0dfb4822b7b0fdbb18ebc5b1cad6fe4e9cb
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-05-11 20:53:27 -0600

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(object) {
+let titles = [];
+for (let i = 0; i < object.length; i++) titles.push(object[i].title);
+return titles;
 };
 
 // Do not edit below this line
