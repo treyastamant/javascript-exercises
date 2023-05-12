c93b2fcaa327c383b0ea8091adc743fa52899e82
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-05-11 20:23:07 -0600

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+let i;
+let fib = [0, 1];
+if (num < 0) {
+  return "OOPS";
+} else {
+  for (i = fib.length; i <= num; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+    console.log(fib);
+  }
+}
+return fib[num];
+
 
 };
 
